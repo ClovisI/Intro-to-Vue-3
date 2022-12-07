c6ed6e3201023a1b6fc4375cb4e789e6d1e564cd
Clovis I <>
2022-12-07 10:04:48 -0700

    L2 completed with coding challenge

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Comfy covers for your feet, made from 100% cotton.'
+    };
+  }
+});
